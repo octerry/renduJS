1d1b30f2367305698cd0677caef8baeb8ba40e79
octerry <taaniyu@gmail.com>
2025-12-17 11:04:12 +0100

    Ajout des exercices html-js

--- interactions/main.js
+++ interactions/main.js
@@ -1,13 +1,47 @@
-let titleElement = document.getElementById('title')
-let imageElement = document.getElementById('image')
+let textInput = document.getElementById('txtInput')
+let para1 = document.getElementById('para1')
+let image = document.getElementById('imageToChange')
+let para2 = document.getElementById('para2')
+let exo4 = document.getElementsByClassName('exo4')
+let exo4Button = document.getElementById('exo4Button')
+let condition = false
 
-titleElement.innerHTML = "Paul qui envoie un beau meme de Squeezie"
-imageElement.src = "https://media1.tenor.com/m/3mWy2uW1DeMAAAAd/wtf-weird.gif"
+textInput.addEventListener("input", function () {
+    para1.textContent = this.value
+});
 
-function changeImage () {
-    if (imageElement.src == "https://media1.tenor.com/m/3mWy2uW1DeMAAAAd/wtf-weird.gif") {
-        imageElement.src = "https://media1.tenor.com/m/zs6ypEf8BNwAAAAd/old-man-spray-paint-bug-eye-man.gif"
+image.addEventListener("click", function () {
+    if (this.src == "https://media1.tenor.com/m/MVlkQDbo2I8AAAAd/smiling-cat-happy-cat.gif") {
+        this.src = "https://media1.tenor.com/m/SUE6m6RW2jMAAAAd/cow-middle-finger.gif"
     } else {
-        imageElement.src = "https://media1.tenor.com/m/3mWy2uW1DeMAAAAd/wtf-weird.gif"
+        this.src = "https://media1.tenor.com/m/MVlkQDbo2I8AAAAd/smiling-cat-happy-cat.gif"
     }
-}
+})
+
+para2.addEventListener("mouseover", function () {
+    para2.innerHTML = "Belle souris :)"
+})
+
+para2.addEventListener("mouseout", function () {
+    para2.innerHTML = "Survol moi"
+})
+
+exo4Button.addEventListener("click", function () {
+    if (condition) {
+        for (const element of exo4) {
+            console.log(element)
+            element.style["background-color"] = "#000"
+            element.style["color"] = "#fff"
+            element.style["border"] = "3px #fff"
+            condition = false
+        }
+    } else {
+        for (const element of exo4) {
+            console.log(element)
+            element.style["background-color"] = "#fff"
+            element.style["color"] = "#000"
+            element.style[""] = "3px #000"
+            condition = true
+        }
+    }
+})
